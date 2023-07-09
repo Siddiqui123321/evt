4575c0f18678835e9bd599f0eb57814018e8eb24
Siddiqui123321 <mohdhumzasid@gmail.com>
2023-07-10 03:41:23 +0530

    register models on admin site

--- lift/liftapi/admin.py
+++ lift/liftapi/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from liftapi.models import Elevator, ElevatorRequest, ElevatorSystem
 # Register your models here.
+
+admin.site.register(Elevator)
+admin.site.register(ElevatorRequest)
+admin.site.register(ElevatorSystem)
